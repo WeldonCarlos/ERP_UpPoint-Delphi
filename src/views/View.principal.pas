46ac2185cb06c485bd97cb6162c797113bd1e132
Weldon Carlos <weldonprogrammer2022@gmail.com>
2024-03-21 16:53:21 -0300

    aplicação hover no logo do topo ,hover no icone do usuario, Fechar o programa apertando a tecla esc, logo central adicionada, icones do menu lateral adcionados usando o actionList e o imagemList

--- src/views/View.principal.pas
+++ src/views/View.principal.pas
@@ -11,7 +11,8 @@ uses
   Vcl.Graphics,
   Vcl.Controls,
   Vcl.Forms,
-  Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls, Vcl.Buttons;
+  Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls, Vcl.Buttons, Vcl.Imaging.pngimage,
+  System.ImageList, Vcl.ImgList, System.Actions, Vcl.ActnList;
 
 type
   TViewPrincipal = class(TForm)
@@ -29,7 +30,7 @@ type
     PanelUsuario: TPanel;
     PanelLineUsuario: TPanel;
     PanelImgUsuario: TPanel;
-    Image1: TImage;
+    imgUserBranca: TImage;
     PanelDadosUsuario: TPanel;
     LblUsuario: TLabel;
     LblPerfil: TLabel;
@@ -48,6 +49,9 @@ type
     BtnFornecedores: TSpeedButton;
     BtnConfigurações: TSpeedButton;
     BtnSair: TSpeedButton;
+    ImgUserLaranja: TImage;
+    PanelConteudoCentro: TPanel;
+    LogoCentro: TImage;
     procedure BtnSairClick(Sender: TObject);
     procedure BtnClientesClick(Sender: TObject);
     procedure BtnCaixaClick(Sender: TObject);
@@ -55,6 +59,11 @@ type
     procedure BtnProdutosClick(Sender: TObject);
     procedure BtnConfiguraçõesClick(Sender: TObject);
     procedure FormShow(Sender: TObject);
+    procedure FormKeyPress(Sender: TObject; var Key: Char);
+    procedure LblTituloEmpresaMouseEnter(Sender: TObject);
+    procedure LblTituloEmpresaMouseLeave(Sender: TObject);
+    procedure imgUserBrancaMouseEnter(Sender: TObject);
+    procedure ImgUserLaranjaMouseLeave(Sender: TObject);
   private
     procedure GET_LineMenu(Sender: TObject);
   public
@@ -96,6 +105,16 @@ end;
 procedure TViewPrincipal.BtnSairClick(Sender: TObject);
 begin
  Application.Terminate;
+
+
+end;
+
+procedure TViewPrincipal.FormKeyPress(Sender: TObject; var Key: Char);
+begin
+  if Key = #27 then
+  begin
+     Application.Terminate;
+  end;
 end;
 
 procedure TViewPrincipal.FormShow(Sender: TObject);
@@ -110,6 +129,28 @@ begin
    ShapeMenu.Height := TSpeedButton(Sender).Height;
    ShapeMenu.Top := TSpeedButton(Sender).top;
    PanelShapeMenu.Repaint;
+end;
+
+procedure TViewPrincipal.imgUserBrancaMouseEnter(Sender: TObject);
+begin
+  ImgUserBranca.Visible := False;
+  ImgUserLaranja.Visible := True;
+end;
+
+procedure TViewPrincipal.ImgUserLaranjaMouseLeave(Sender: TObject);
+begin
+   ImgUserBranca.Visible := True;
+  ImgUserLaranja.Visible := False;
+end;
+
+procedure TViewPrincipal.LblTituloEmpresaMouseEnter(Sender: TObject);
+begin
+ LblTituloEmpresa.Font.Color := $000FBBD9;
+end;
+
+procedure TViewPrincipal.LblTituloEmpresaMouseLeave(Sender: TObject);
+begin
+ LblTituloEmpresa.Font.Color := clWhite;
 end;
 
 end.
